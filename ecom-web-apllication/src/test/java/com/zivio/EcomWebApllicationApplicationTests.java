db86103d8b9fb435cffd5cf693b2b45696cdb446
adsulsujit2006-cloud <adsulsujit2006@gmail.com>
2026-06-01 13:38:43 +0530

    first commit

---
+++ ecom-web-apllication/src/test/java/com/zivio/EcomWebApllicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zivio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomWebApllicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
