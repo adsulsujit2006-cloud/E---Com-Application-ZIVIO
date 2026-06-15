10cab20332030e4c440f6d85247af3c27400932e
adsulsujit2006-cloud <adsulsujit2006@gmail.com>
2026-06-15 19:33:10 +0530

    Create PaymentService interface and implementation, then design Order, Payment, and SellerOrder controllers with required methods

--- ecom-web-apllication/src/main/java/com/zivio/controller/OrderController.java
+++ ecom-web-apllication/src/main/java/com/zivio/controller/OrderController.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.razorpay.PaymentLink;
 import com.zivio.Service.CartService;
 import com.zivio.Service.OrderService;
+import com.zivio.Service.PaymentService;
 import com.zivio.Service.SellerReportService;
 import com.zivio.Service.SellerService;
 import com.zivio.Service.UserService;
@@ -24,9 +27,11 @@ import com.zivio.model.Address;
 import com.zivio.model.Cart;
 import com.zivio.model.Order;
 import com.zivio.model.OrderItem;
+import com.zivio.model.PaymentOrder;
 import com.zivio.model.Seller;
 import com.zivio.model.SellerReport;
 import com.zivio.model.User;
+import com.zivio.repository.PaymentOrderRepository;
 import com.zivio.responce.PaymentLinkResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -41,63 +46,96 @@ public class OrderController {
     private final CartService cartService;
     private final SellerService sellerService;
     private final SellerReportService sellerReportService;
+    private final PaymentService paymentService;
+    private final PaymentOrderRepository paymentOrderRepository;
 
+    @PostMapping()
+    public ResponseEntity<PaymentLinkResponse> createOrderHandler(
+            @RequestBody Address shippingAddress,
+            @RequestParam PaymentMethod paymentMethod,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
 
-  @PostMapping()
-public ResponseEntity<PaymentLinkResponse> createOrderHandler(
-        @RequestBody Address shippingAddress,
-        @RequestParam PaymentMethod paymentMethod,
-        @RequestHeader("Authorization") String jwt
-) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        Cart cart = cartService.findUserCart(user);
+        Set<Order> orders = orderService.createOrder(user, shippingAddress, cart);
 
-   User user = userService.findUserByJwtToken(jwt);
-   Cart cart = cartService.findUserCart(user);
-   Set<Order> orders = orderService.createOrder(user, shippingAddress, cart);
+        PaymentOrder paymentOrder = paymentService.createOrder(user, orders);
+        PaymentLinkResponse res = new PaymentLinkResponse();
 
-   PaymentLinkResponse res = new PaymentLinkResponse();
-  
+        if (paymentMethod.equals(PaymentMethod.RAZORPAY)) {
 
-   return new ResponseEntity<>(res,HttpStatus.OK);
-}
+            PaymentLink payment = paymentService.createRazorpayPaymentLink(
+                    user,
+                    paymentOrder.getAmount(),
+                    paymentOrder.getId()
+            );
 
-@GetMapping("/user")
-public ResponseEntity<List<Order>> userOrderHistoryHandler(
-    @RequestHeader("Authorization") String jwt
-) throws Exception{
+            String paymentUrl = payment.get("short_url");
+            String paymentUrlId = payment.get("id");
 
-    User user = userService.findUserByJwtToken(jwt);
-    List<Order> orders = orderService.userOrderHistory(user.getId());
+            res.setPayment_link_url(paymentUrl);
+            res.setPayment_link_id(paymentUrlId);
 
-    
-    return new ResponseEntity<>(orders,HttpStatus.ACCEPTED);
-    
-}
-@GetMapping("/{orderId}")
-public ResponseEntity<OrderItem> getOrderById(
-    @PathVariable Long orderItemId, @RequestHeader("Authorization") String jwt
-) throws Exception{
-    User user = userService.findUserByJwtToken(jwt);
-    OrderItem orderItem = orderService.getOrderItemById(orderItemId);
-    return new ResponseEntity<>(orderItem,HttpStatus.ACCEPTED);
-}
+            paymentOrder.setPaymentLinkId(paymentUrlId);
+            paymentOrderRepository.save(paymentOrder);
 
-public ResponseEntity<Order> cancleOrder(
-    @PathVariable Long orderId,
-    @RequestHeader("Authorization") String jwt
-) throws Exception{
-    User user = userService.findUserByJwtToken(jwt);
-    Order order = orderService.cancleOrder(orderId, user);
+        } else {
 
-    Seller seller = sellerService.getSellerById(order.getSellerId());
-    SellerReport report = sellerReportService.getSellerReport(seller);
+            String paymentUrl = paymentService.createStripePaymentLink(
+                    user,
+                    paymentOrder.getAmount(),
+                    paymentOrder.getId()
+            );
 
-    report.setCancledOrders(report.getCancledOrders()+1);
-    report.setTotalRefunds(report.getTotalRefunds()+order.getTotalSellingPrice());
-    sellerReportService.updateSellerReport(report);
+            res.setPayment_link_url(paymentUrl);
+        }
 
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
 
-    return ResponseEntity.ok(order);
-    
-}
+    @GetMapping("/user")
+    public ResponseEntity<List<Order>> userOrderHistoryHandler(
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
 
+        User user = userService.findUserByJwtToken(jwt);
+        List<Order> orders = orderService.userOrderHistory(user.getId());
+
+        return new ResponseEntity<>(orders, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/{orderItemId}")
+    public ResponseEntity<OrderItem> getOrderById(
+            @PathVariable Long orderItemId,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+
+        OrderItem orderItem = orderService.getOrderItemById(orderItemId);
+
+        return new ResponseEntity<>(orderItem, HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{orderId}/cancel")
+    public ResponseEntity<Order> cancleOrder(
+            @PathVariable Long orderId,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+
+        Order order = orderService.cancleOrder(orderId, user);
+
+        Seller seller = sellerService.getSellerById(order.getSellerId());
+        SellerReport report = sellerReportService.getSellerReport(seller);
+
+        report.setCancledOrders(report.getCancledOrders() + 1);
+        report.setTotalRefunds(report.getTotalRefunds() + order.getTotalSellingPrice());
+
+        sellerReportService.updateSellerReport(report);
+
+        return ResponseEntity.ok(order);
+    }
 }
